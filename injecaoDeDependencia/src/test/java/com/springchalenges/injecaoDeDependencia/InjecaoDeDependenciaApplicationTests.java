3fcafd1777655a2cc3c4c9c001713e14bc472c1f
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2025-03-02 15:22:23 -0300

    first commit

---
+++ injecaoDeDependencia/src/test/java/com/springchalenges/injecaoDeDependencia/InjecaoDeDependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springchalenges.injecaoDeDependencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjecaoDeDependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
