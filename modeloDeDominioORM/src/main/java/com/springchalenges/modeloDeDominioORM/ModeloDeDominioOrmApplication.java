5514040fc6a143b0c0a66944ead32de10869a0ec
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2025-03-04 18:53:04 -0300

    Adicionando novo projeto de desafio

---
+++ modeloDeDominioORM/src/main/java/com/springchalenges/modeloDeDominioORM/ModeloDeDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.springchalenges.modeloDeDominioORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloDeDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloDeDominioOrmApplication.class, args);
+	}
+
+}
