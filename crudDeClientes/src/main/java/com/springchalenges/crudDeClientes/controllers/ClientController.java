079c14d65e88d0279bd1ac81d884da618c629bb5
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2025-03-09 12:27:25 -0300

    Adicionando atualização e deleção de cliente

--- crudDeClientes/src/main/java/com/springchalenges/crudDeClientes/controllers/ClientController.java
+++ crudDeClientes/src/main/java/com/springchalenges/crudDeClientes/controllers/ClientController.java
@@ -32,8 +32,21 @@ public class ClientController {
     @PostMapping
     public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto) {
         dto = service.insert(dto);
+
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/${id}").buildAndExpand(dto.getId()).toUri();
 
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @Valid @RequestBody ClientDTO dto) {
+        return ResponseEntity.ok(service.update(id, dto));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- crudDeClientes/src/main/java/com/springchalenges/crudDeClientes/services/ClientService.java
+++ crudDeClientes/src/main/java/com/springchalenges/crudDeClientes/services/ClientService.java
@@ -3,11 +3,15 @@ package com.springchalenges.crudDeClientes.services;
 import com.springchalenges.crudDeClientes.dto.ClientDTO;
 import com.springchalenges.crudDeClientes.entities.Client;
 import com.springchalenges.crudDeClientes.repositories.ClientRepository;
+import com.springchalenges.crudDeClientes.services.exceptions.DatabaseException;
 import com.springchalenges.crudDeClientes.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -30,12 +34,46 @@ public class ClientService {
     public ClientDTO insert(ClientDTO dto) {
         Client entity = new Client();
 
+        copyClientDtoToEntity(entity, dto);
+
+        return new ClientDTO(repository.save(entity));
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        try {
+            Client entity = repository.getReferenceById(id);
+
+            copyClientDtoToEntity(entity, dto);
+
+            repository.save(entity);
+
+            return new ClientDTO(entity);
+        } catch (EntityNotFoundException e) {
+            e.printStackTrace();
+            throw new ResourceNotFoundException("Cliente não encontrado");
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Cliente não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
+
+    private void copyClientDtoToEntity(Client entity, ClientDTO dto) {
         entity.setName(dto.getName());
         entity.setCpf(dto.getCpf());
         entity.setBirthDate(dto.getBirthDate());
         entity.setIncome(dto.getIncome());
         entity.setChildren(dto.getChildren());
-
-        return new ClientDTO(repository.save(entity));
     }
+
 }
