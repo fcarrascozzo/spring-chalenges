7977c19fbdacd9e29a9800c33b493e3246d2cc68
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2025-03-08 22:30:40 -0300

    Criando projeto de crud

---
+++ crudDeClientes/src/main/java/com/springchalenges/crudDeClientes/CrudDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.springchalenges.crudDeClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDeClientesApplication.class, args);
+	}
+
+}
